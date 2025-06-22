9b26da663edb55e92ffd0df50d4f2fd5c599af2d
ErickGC546 <erick.galindo.c@tecsup.edu.pe>
2025-06-22 00:10:39 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'iOS Task Manager',
       theme: CupertinoThemeData(
-        primaryColor: CupertinoColors.activeBlue,
+        primaryColor: const Color.fromARGB(255, 41, 45, 48),
         scaffoldBackgroundColor: CupertinoColors.systemGroupedBackground,
         barBackgroundColor: CupertinoColors.systemGrey6,
       ),
